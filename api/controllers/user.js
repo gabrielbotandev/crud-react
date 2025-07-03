416f0f3636fe4aee2943f35620e2443741f5b5c4
gabrielbotandev <gabrielficciobotan@gmail.com>
2025-07-03 17:03:38 -0300

    :sparkles: feat: Add user management endpoints for creating, updating, and deleting users in the API.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -9,3 +9,56 @@ export const getUsers = (_, res) => {
     return res.status(200).json(data)
   })
 }
+
+export const addUser = (req, res) => {
+  const q =
+    'INSERT INTO usuarios(`nome`, `email`, `tel`, `data_nascimento`) VALUES(?)'
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.tel,
+    req.body.data_nascimento,
+  ]
+
+  db.query(q, [values], (err) => {
+    if (err) return res.json(err)
+
+    return res.status(200).json('Usuário criado com sucesso.')
+  })
+}
+
+export const updateUser = (req, res) => {
+  const q =
+    'UPDATE usuarios SET `nome` = ?, `email` = ?, `tel` = ?, `data_nascimento` = ? WHERE `id` = ?'
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.tel,
+    req.body.data_nascimento,
+  ]
+
+  db.query(q, [...values, req.params.id], (err) => {
+    if (err) return res.json(err)
+
+    return res.status(200).json('Usuário atualizado com sucesso.')
+  })
+}
+
+export const deleteUser = (req, res) => {
+  const q = 'DELETE FROM usuarios WHERE `id` = ?'
+
+  const values = [
+    req.body.nome,
+    req.body.email,
+    req.body.tel,
+    req.body.data_nascimento,
+  ]
+
+  db.query(q, [req.params.id], (err) => {
+    if (err) return res.json(err)
+
+    return res.status(200).json('Usuário deletado com sucesso.')
+  })
+}
